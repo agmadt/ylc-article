808be680d2a38e07b6a68a6b0e3be842e47af55e
Agam Adhityo <adhityoagam@gmail.com>
2020-05-20 14:59:02 +0700

    update feedback

--- js/script.js
+++ js/script.js
@@ -14,10 +14,27 @@ $(document).ready(function() {
                     'position': 'relative'
                 })
             }
-
         } else {
             sidebarEl.css({
                 'position': 'static'
+            })
+        }
+    })
+
+    $(window).resize(function() {
+        var windowWidth = $(window).width();
+
+
+        if (windowWidth < 769 && windowWidth > 320) {
+            var featureImageNegativeMargin = (windowWidth - 288) / 2;
+            $('.feature-image').css({
+                'margin-left': -featureImageNegativeMargin,
+                'margin-right': -featureImageNegativeMargin
+            })
+        } else {
+            $('.feature-image').css({
+                'margin-left': 0,
+                'margin-right': 0
             })
         }
     })
